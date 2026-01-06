fb1403fb808597b5d745044c357145e82829ea6d
Iksboi <130296326+Iksboi@users.noreply.github.com>
2026-01-06 11:43:39 +0000

    Everything is in order, Had to deprecate to 14

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -12,19 +12,19 @@ export default function Navigation() {
       <div className={"container " + (active ? "active" : "")}>
         <ul>
           <li>
-            <Link href="/">
-              <a className={router.pathname === "/" ? "active" : null}>about</a>
+            <Link href="/" className={router.pathname === "/" ? "active" : null}>
+              about
             </Link>
           </li>
           <li>
-            <Link href="/posts">
-              <a
-                className={
-                  router.pathname.startsWith("/posts") ? "active" : null
-                }
-              >
+            <Link
+              href="/posts"
+              className={
+                router.pathname.startsWith("/posts") ? "active" : null
+              }>
+              
                 blog
-              </a>
+              
             </Link>
           </li>
         </ul>
